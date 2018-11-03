52e219c80577da640196f6c278086e8fa26c3e1c
dstark88 <starkwalker88@gmail.com>
2018-11-03 11:31:48 -0600

    working on search page

--- parallax-template/js/init.js
+++ parallax-template/js/init.js
@@ -18,3 +18,28 @@
   };
   firebase.initializeApp(config);
 // end firebase
+
+// event listener for the drop down search options
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('select');
+  var instances = M.FormSelect.init(elems, options);
+});
+
+// Or with jQuery
+
+$(document).ready(function(){
+  $('select').formSelect();
+});
+
+var instance = M.FormSelect.getInstance(elem);
+
+/* jQuery Method Calls
+  You can still use the old jQuery plugin method calls.
+  But you won't be able to access instance properties.
+
+  $('select').formSelect('methodName');
+  $('select').formSelect('methodName', paramName);
+*/
+
+instance.getSelectedValues();
+// end drop downs

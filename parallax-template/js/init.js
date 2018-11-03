64a586138d595cc7fdf8e62e3d58bd0b7bd6324e
Ginger KRC <gingerkr@gmail.com>
2018-11-03 13:53:43 -0600

    js changes

--- parallax-template/js/init.js
+++ parallax-template/js/init.js
@@ -1,36 +1,5 @@
-function generateSearch() {
-
-  var locSearch = $(this).attr("autocomplete-input");
-  var locQueryURL = "https://www.loc.gov/books/?q=" +
-  locSearch + "fo=json";
-
-  var openSearch = $(this).attr("autocomplete-input");
-  var openQueryURL = "http://openlibrary.org/search.json?q=" +
-  openSearch;
-
-// if () {
-// }
-
-$.ajax({
-  url: locQueryURL,
-  method: "GET"
-})
-  .then(function(response) { 
-    var locResults = response.data;
-    console.log(response);
-  }
-
-  $.ajax({
-    url: openQueryURL,
-    method: "GET"
-  })
-    .then(function(response) { 
-      var openResults = response.data;
-      console.log(response);
-    }
-  
-  (function($){
-  $(function(){
+(function ($) {
+  $(function () {
 
     $('.sidenav').sidenav();
     $('.parallax').parallax();
@@ -38,39 +7,70 @@ $.ajax({
   }); // end of document ready
 })(jQuery); // end of jQuery name space
 
-  // Firebase information and initialization
-  var config = {
-    apiKey: "AIzaSyCREFeSA5eZ1W_Wg4Iw-8CJJfYAxPzzL70",
-    authDomain: "dontforkthisup.firebaseapp.com",
-    databaseURL: "https://dontforkthisup.firebaseio.com",
-    projectId: "dontforkthisup",
-    storageBucket: "dontforkthisup.appspot.com",
-    messagingSenderId: "1092097473350"
-  };
-  firebase.initializeApp(config);
+// Firebase information and initialization
+var config = {
+  apiKey: "AIzaSyCREFeSA5eZ1W_Wg4Iw-8CJJfYAxPzzL70",
+  authDomain: "dontforkthisup.firebaseapp.com",
+  databaseURL: "https://dontforkthisup.firebaseio.com",
+  projectId: "dontforkthisup",
+  storageBucket: "dontforkthisup.appspot.com",
+  messagingSenderId: "1092097473350"
+};
+firebase.initializeApp(config);
 // end firebase
+function generateSearch() {
 
-// event listener for the drop down search options
-document.addEventListener('DOMContentLoaded', function() {
-  var elems = document.querySelectorAll('select');
-  var instances = M.FormSelect.init(elems, options);
-});
+  var locSearch = $(this).attr("autocomplete-input");
+  var locQueryURL = "https://www.loc.gov/books/?q=" +
+    locSearch + "fo=json";
 
-// Or with jQuery
+  var openSearch = $(this).attr("autocomplete-input");
+  var openQueryURL = "http://openlibrary.org/search.json?q=" +
+    openSearch;
 
-$(document).ready(function(){
-  $('select').formSelect();
-});
+  // if () {
+  // }
 
-var instance = M.FormSelect.getInstance(elem);
+  $.ajax({
+    url: locQueryURL,
+    method: "GET"
+  })
+    .then(function (response) {
+      var locResults = response.data;
+      console.log(response);
+    });
 
-/* jQuery Method Calls
-  You can still use the old jQuery plugin method calls.
-  But you won't be able to access instance properties.
+  $.ajax({
+    url: openQueryURL,
+    method: "GET"
+  })
+    .then(function (response) {
+      var openResults = response.data;
+      console.log(response);
+    });
 
-  $('select').formSelect('methodName');
-  $('select').formSelect('methodName', paramName);
-*/
+  // event listener for the drop down search options
+  document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('select');
+    var instances = M.FormSelect.init(elems, options);
+  });
 
-instance.getSelectedValues();
+  // Or with jQuery
+
+  $(document).ready(function () {
+    $('select').formSelect();
+  });
+
+  var instance = M.FormSelect.getInstance(elem);
+
+  /* jQuery Method Calls
+    You can still use the old jQuery plugin method calls.
+    But you won't be able to access instance properties.
+  
+    $('select').formSelect('methodName');
+    $('select').formSelect('methodName', paramName);
+  */
+
+  instance.getSelectedValues();
 // end drop downs
+}
